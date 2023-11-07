9c0b027b67a291109fa35a7a01f2e83885c8b378
AdrianRamirez23 <adrian.ramirez23@hotmail.com>
2023-11-07 15:25:07 -0500

    create solution go1 proyect

---
+++ CodeTest/Controllers/HomeController.cs
@@ -0,0 +1,121 @@
+﻿
+using PruebaIngreso.Models;
+using Quote.Contracts;
+using Quote.Models;
+using RestSharp;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+
+
+namespace PruebaIngreso.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IQuoteEngine quote;
+        private readonly IMarginProvider marginProvider;
+
+        public HomeController(IQuoteEngine quote, IMarginProvider marginProvider)
+        {
+            this.quote = quote;
+            this.marginProvider = marginProvider;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            var request = new TourQuoteRequest
+            {
+                adults = 1,
+                ArrivalDate = DateTime.Now.AddDays(1),
+                DepartingDate = DateTime.Now.AddDays(2),
+                getAllRates = true,
+                GetQuotes = true,
+                RetrieveOptions = new TourQuoteRequestOptions
+                {
+                    GetContracts = true,
+                    GetCalculatedQuote = true,
+                },
+                TourCode = "E-U10-PRVPARKTRF",
+                Language = Language.Spanish
+            };
+
+            var result = this.quote.Quote(request);
+            var tour = result.Tours.FirstOrDefault();
+            ViewBag.Message = "Test 1 Correcto";
+            return View(tour);
+        }
+
+        public ActionResult Test2()
+        {
+            ViewBag.Message = "Test 2 Correcto";
+            return View();
+        }
+
+        public async Task<ActionResult> Test3()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Test3(Test3Post test)
+        {
+            if (ModelState.IsValid) 
+            {
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                var client = new RestClient(@"https://refactored-pancake.free.beeceptor.com/");
+                var request = new RestRequest($"/margin/{test.Code}", Method.GET);
+                var response = client.Execute(request);
+
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    ViewBag.Message = "{ \"margin\": 0.0}";
+                }
+                else
+                {
+                    ViewBag.Message = response.Content;
+                }
+            }
+            
+
+            return View(test);
+        }
+
+        public async Task<ActionResult> Test4()
+        {
+            var request = new TourQuoteRequest
+            {
+                adults = 1,
+                ArrivalDate = DateTime.Now.AddDays(1),
+                DepartingDate = DateTime.Now.AddDays(2),
+                getAllRates = true,
+                GetQuotes = true,
+                RetrieveOptions = new TourQuoteRequestOptions
+                {
+                    GetContracts = true,
+                    GetCalculatedQuote = true,
+                },
+                TourCode = "E-U10-PRVPARKTRF",
+                Language = Language.Spanish
+            };
+
+            var result = this.quote.Quote(request);
+
+            foreach(var tourQuote in result.TourQuotes) 
+            {
+                tourQuote.margin = await marginProvider.GetMarginAsync(tourQuote.ContractCode);
+            }
+            
+            return View(result.TourQuotes);
+        }
+    }
+}
---
+++ CodeTest/Models/Test3Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PruebaIngreso.Models
+{
+    public class Test3Post
+    {
+        [Required(ErrorMessage = "El campo código es requerido")]
+        [Display(Name = "Código")]
+        public string Code { get; set; }
+    }
+}
